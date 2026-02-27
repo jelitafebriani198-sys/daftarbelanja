34be660da97178132abffb1395f143443dd3eb17
jelitafebriani198-sys <jelitafebriani198@gmail.com>
2026-02-27 11:18:32 +0700

    Add files via upload

---
+++ coba_api.py
@@ -0,0 +1,35 @@
+import urllib.request
+import json
+
+urls = [
+    "http://api.open-notify.org/astros.json",
+    "http://httpbin.org/get",
+    "https://jsonplaceholder.typicode.com/post"
+]
+
+for url in urls:
+    try:
+        print(f"Mencoba {url}...")
+        response = urllib.request.urlopen(url, timeout=5)
+        data_json = response.read().decode('utf-8')
+        data = json.loads(data_json)
+        print("✅ Berhasil!\n")
+      
+        # Tampilkan data sesuai URL
+        if "astros" in url:
+          print(f"Astronout di ISS: {data['number']} orang")
+          for astronaut in data['people']:
+            print(f" - {astronaut['name']}")
+        elif "httpbin" in url:
+             print(f"Origin IP: {data['origin']}")
+        elif "jsonplaceholder" in url:
+            print("Contoh judul:", data[0]['title'])
+        break
+    except Exception as e:
+        print(f"Gagal: {e}\n")
+else:
+    print("semua URL gagal. Gunakan data lokal.")
+    data = [{"title": "Belajar API"}, {"title": "Error Handling"}, {"title": "Logging"}]
+    print("\nyata lokal:")
+    for i, post in enumerate(data, start=1):
+        print(f"{i}. {post['title']}")
